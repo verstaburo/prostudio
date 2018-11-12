6f692b896eedcec7dacac576b81e1f23b5b59216
truemoroz <aS!oi%1l4l>
2018-11-12 12:58:22 +0400

    завершил портфолио

--- app/pages/portfolio/portfolio.js
+++ app/pages/portfolio/portfolio.js
@@ -2,27 +2,18 @@ const $ = window.$;
 
 export default function portfolioFilter() {
   $('.js-portfolio-filer').click((e) => {
-    // console.log(e.target);
-    // const self = this;
-    const filterTag = $(e.target).html();
-    // console.log(filterTag);
-    // $('.card-portfolio').each((index, element) => {
-    $('.card-portfolio').filter((index, element) => {
-      // return $("strong", this).length === 1;
-      // return 0;
-      // console.log(index);
-      // $(element).
-      // const item = $(this);
-      const tag = $(element).find('.card-portfolio__tags').html();
-      // if (tag.indexOf(filterTag) >= 0) {
-      //   console.log('совпадение есть!');
-      // }
-      return !(tag.indexOf(filterTag) >= 0);
-      // console.log(tag);
-      // console.log($('.card-portfolio__tags', this).html());
-      // return index % 3 === 2;
-      // return index % 3 === 2;
-    }).hide();
-    // $('.card-portfolio').filter('.card-portfolio_wide').hide();
+    const self = e.target;
+    $(self).addClass('portfolio__filter_active').siblings().removeClass('portfolio__filter_active');
+    const filterTag = $(self).html();
+    $('.card-portfolio').hide();
+
+    if (filterTag !== 'Все') {
+      $('.card-portfolio').filter((index, element) => {
+        const tag = $(element).find('.card-portfolio__tags').html();
+        return (tag.indexOf(filterTag) >= 0);
+      }).show(300);
+    } else {
+      $('.card-portfolio').show(300);
+    }
   });
 }
