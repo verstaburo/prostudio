4e6eeceda9c21f00081840b51d27402649eba432
truemoroz <moroz0@gmail.com>
2018-11-12 07:53:07 +0400

    фильтрация портфолио

--- app/pages/portfolio/portfolio.js
+++ app/pages/portfolio/portfolio.js
@@ -2,21 +2,22 @@ const $ = window.$;
 
 export default function portfolioFilter() {
   $('.js-portfolio-filer').click((e) => {
-    console.log(e);
+    // console.log(e.target);
     // const self = this;
-    const filterTag = $(this).html();
+    const filterTag = $(e.target).html();
+    // console.log(filterTag);
     // $('.card-portfolio').each((index, element) => {
     $('.card-portfolio').filter((index, element) => {
       // return $("strong", this).length === 1;
       // return 0;
       // console.log(index);
-              // $(element).
+      // $(element).
       // const item = $(this);
       const tag = $(element).find('.card-portfolio__tags').html();
-      if (tag.indexOf(filterTag) >= 0) {
-        console.log('совпадение есть!');
-      }
-      return $('.card-portfolio__test', element).length === 1;
+      // if (tag.indexOf(filterTag) >= 0) {
+      //   console.log('совпадение есть!');
+      // }
+      return !(tag.indexOf(filterTag) >= 0);
       // console.log(tag);
       // console.log($('.card-portfolio__tags', this).html());
       // return index % 3 === 2;
