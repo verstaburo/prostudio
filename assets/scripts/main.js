f690b180e1ba54ede98c23a15473fe9fe2e045c4
Gleb Gorokhov <gg@verstaburo.com>
2019-01-16 21:29:10 +0400

    Update 2019-01-16T17:29:01.249Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,6 +11,60 @@
 
 $(document).ready(() => {
 
+  function checkMenuLinks() {
+    var links = document.querySelectorAll('.menu__item .link'),
+          loc = window.location.href;
+
+    for (var i = 0; i < links.length; i++) {
+      links[i].href == loc ? links[i].classList.add('is-active') : false;
+    }
+  }
+  checkMenuLinks();
+
+  function switchLanguage() {
+    var ckeckbox = document.querySelectorAll('.js-switcher-lang input[type="checkbox"]'),
+        elem = document.querySelectorAll('.js-switcher-lang .switcher__value'),
+        elemValue = elem[elem.length - 1].innerHTML.toLowerCase(),
+        loc = window.location;
+
+    for (var i = 0; i < ckeckbox.length; i++) {
+      if (ckeckbox[i].checked) {
+        loc.href = loc.origin + '/' + elemValue + loc.pathname;
+      }
+    }
+  }
+
+  $('.js-switcher-lang input[type="checkbox"]').click(() => {
+    switchLanguage();
+  });
+
+  $('.langs .langs__lang').click((event) => {
+    var self = event.currentTarget,
+        value = $(self).text().toLowerCase(),
+        activeClass = 'langs__lang_active',
+        loc = window.location;
+
+    $(self).addClass(activeClass).siblings().removeClass(activeClass);
+
+    if (value == 'en') {
+      loc.href = loc.origin + '/' + value + loc.pathname;
+    }
+  });
+
+  $('.buttons__burger').click(() => {
+    var header = document.querySelector('.header'),
+        headerFixed = header.querySelector('.header_fixed'),
+        bodyDark = document.querySelector('.body_dark');
+    if (!header.contains(headerFixed)) {
+      $('.header').toggleClass('header_fixed-light');
+      $('.header').toggleClass('header_no-border');
+    }
+    if (document.body.contains(bodyDark)) {
+      $('.header').toggleClass('header_fixed-dark');
+      $('.header').toggleClass('header_no-border');
+    }
+  });
+
   $('.js-padmenu').click(() => {
     $('.buttons__burger').toggleClass('buttons__burger_active');
     $('.header__menu').toggleClass('header__menu_active');
